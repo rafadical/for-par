20697e07d7955d3ebf25c8e5637853db13d899ce
rafael maruca <rafadicalpvp@gmail.com>
2022-07-06 18:54:52 -0300

    Adicionar arquivos de projeto.

---
+++ for par/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_par
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            int i;
+            for (i = 2; i <= 100; i+=2)
+
+
+
+                Console.WriteLine(i);
+
+
+
+            {
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
